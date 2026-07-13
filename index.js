2260669191b401edda79fd00133f860c07fc7431
Rohan <rk2024@duck.com>
2026-07-13 15:20:32 +0530

    feat: Implemented GET '/leads/:id/comments' api endpoint for fetching all Comments for a particular lead.

--- index.js
+++ index.js
@@ -291,7 +291,7 @@ const createComment = async (commentData) => {
 
 app.post("/leads/:id/comments", async (req, res) => {
   try {
-    const {id} = req.params;
+    const { id } = req.params;
 
     if (id && !mongoose.Types.ObjectId.isValid(id)) {
       return res
@@ -309,6 +309,39 @@ app.post("/leads/:id/comments", async (req, res) => {
     if (error.name === "ValidationError") {
       return res.status(400).json({ error: error.message });
     }
+    res.status(500).json({ error: error.message });
+  }
+});
+
+//get comments data using mongoose
+
+const getAllComments = async () => {
+  try {
+    return await commentModel.find();
+  } catch (error) {
+    console.error("error", error.message);
+  }
+};
+
+//get comments data using express api
+
+app.get("/leads/:id/comments", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    if (id && !mongoose.Types.ObjectId.isValid(id)) {
+      return res
+        .status(400)
+        .json({ error: `Lead With Id '${id}' not a valid Object Id.` });
+    }
+    const comments = await getAllComments();
+    if (id && !comments) {
+      return res.status(404).json({ error: `Lead with Id '${id}' not found.` });
+    }
+    res
+      .status(200)
+      .json({ message: "Comments fetched successfully.", comments });
+  } catch (error) {
     res.status(500).json({ error: error.message });
   }
 });
